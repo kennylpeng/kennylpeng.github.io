c88862a5f25e6b719325b6840dbdb96f2503603b
kennylpeng <38672073+kennylpeng@users.noreply.github.com>
2019-06-16 21:21:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,9 @@
 // will see an error in the editor
 /* global Chart */
 
+Chart.defaults.global.defaultFontFamily = "'Lato'";
+Chart.defaults.global.defaultFontColor = 'red';
+
 // create an object with world population data
 const data = {
   'Africa': 1216,
